cc6f4cec4952bec5fa77fa8d53163b59a8f00b44
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 14:52:13 +0200

    creating class PostTag

---
+++ src/main/java/com/example/forumproject/models/PostTag.java
@@ -0,0 +1,43 @@
+package com.example.forumproject.models;
+
+import jakarta.persistence.*;
+
+
+@Table(name = "post_tags")
+public class PostTag {
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    public PostTag() {
+    }
+
+    public PostTag(Post post, Tag tag) {
+        this.post = post;
+        this.tag = tag;
+    }
+
+
+
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
+}
