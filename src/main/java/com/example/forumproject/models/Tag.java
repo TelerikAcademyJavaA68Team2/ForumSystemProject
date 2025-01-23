ff1f712660dc8cf21c0cf07f34f3bd7792772c7a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 14:52:24 +0200

    creating class Tag

---
+++ src/main/java/com/example/forumproject/models/Tag.java
@@ -0,0 +1,40 @@
+package com.example.forumproject.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String tagName;
+
+    public Tag() {
+
+    }
+
+    public Tag(int id, String tagName) {
+        this.id = id;
+        this.tagName = tagName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+}
