cae201cb9a6728cf221f07b483ec44a5a8990935
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 21:28:32 +0200

    adding EntityNotFoundException

---
+++ src/main/java/com/example/forumproject/exceptions/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.forumproject.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String type, String attribute, String value) {
+        super(String.format("%s with %s %s not found.", type, attribute, value));
+    }
+
+    public EntityNotFoundException(String type, int id) {
+        super(String.format("%s with id %d not found.", type, id));
+    }
+}
