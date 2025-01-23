eb6f653f7be0fa0d2056ff978179aefc0864b2f0
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 21:29:04 +0200

    adding methods to UserRepository interface

--- src/main/java/com/example/forumproject/repositorys/UserRepository.java
+++ src/main/java/com/example/forumproject/repositorys/UserRepository.java
@@ -1,4 +1,40 @@
 package com.example.forumproject.repositorys;
 
+import com.example.forumproject.models.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    List<User> getAllUsers();
+
+    void createUser(User user);
+
+    User getById(int userId);
+
+    User getByEmail(String email);
+
+    User getByUsername(String username);
+
+    void updateUser(User user);
+
+    void deleteUser(int userId);
+
+    void promoteToAdmin(int userId);
+
+    void blockUser(int userId);
+
+    void unblockUser(int userId);
+
+    // Search and filtering
+/*    List<User> searchUsers(String firstName, String lastName, String email);
+
+    List<Post> findUsersPosts(Long userId, PostFilterCriteria filter);
+
+    // Authentication and profile management
+    User authenticateUser(String usernameOrEmail, String password);
+
+    void updateUserProfile(User user);
+
+    void uploadProfilePicture(Long userId, String imagePath);*/
 }
