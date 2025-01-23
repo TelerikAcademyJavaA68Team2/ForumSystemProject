0ecff6e5034fde9446bde0dd1a8bdd44ef6996a1
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 14:51:59 +0200

    creating class Post

---
+++ src/main/java/com/example/forumproject/models/Post.java
@@ -0,0 +1,80 @@
+package com.example.forumproject.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "likes_count")
+    private int likesCount;
+
+    public Post() {
+    }
+
+    public Post(int id,
+                User author,
+                String title,
+                String content,
+                int likesCount) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.likesCount = likesCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+}
