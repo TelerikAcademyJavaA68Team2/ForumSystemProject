d56f7c087534c8f64f827ed0d9567d068dc4ef88
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 14:54:19 +0200

    creating interface UserService

---
+++ src/main/java/com/example/forumproject/services/UserService.java
@@ -0,0 +1,20 @@
+package com.example.forumproject.services;
+
+
+import com.example.forumproject.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAll();
+
+    User getUserById(int id);
+
+    User getByEmail(String email);
+
+    void create(User user);
+
+    void update(User user, int id);
+
+    void delete(int id);
+}
