43068a0e542509e8fbdf5af9113d28ff121512ca
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 14:53:11 +0200

    creating class UserServiceImpl

---
+++ src/main/java/com/example/forumproject/services/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.forumproject.services;
+
+import com.example.forumproject.models.User;
+import com.example.forumproject.repositorys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public List<User> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public void update(User user, int id) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
