4fdba672f9ab3ab66481d97d19462400b6f22dd8
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 21:29:28 +0200

    implementing methods inUserRepositoryImpl

--- src/main/java/com/example/forumproject/repositorys/UserRepositoryImpl.java
+++ src/main/java/com/example/forumproject/repositorys/UserRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.example.forumproject.repositorys;
 
+import com.example.forumproject.exceptions.EntityNotFoundException;
+import com.example.forumproject.models.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
@@ -15,7 +20,59 @@ public class UserRepositoryImpl implements UserRepository {
     }
 
 
+    @Override
+    public List<User> getAllUsers() {
+        return List.of();
+    }
 
+    @Override
+    public void createUser(User user) {
 
+    }
 
+    @Override
+    public User getById(int userId) {
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.get(User.class, userId);
+            if (user == null) {
+                throw new EntityNotFoundException("Beer", userId);
+            }
+            return user;
+        }
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+
+    }
+
+    @Override
+    public void promoteToAdmin(int userId) {
+
+    }
+
+    @Override
+    public void blockUser(int userId) {
+
+    }
+
+    @Override
+    public void unblockUser(int userId) {
+
+    }
 }
