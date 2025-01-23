5af23643226ae4e251df91a14e3c95c2e11c2c08
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-23 14:51:48 +0200

    creating class Comment

---
+++ src/main/java/com/example/forumproject/models/Comment.java
@@ -0,0 +1,65 @@
+package com.example.forumproject.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column
+    private String content;
+
+    public Comment() {
+    }
+
+    public Comment(int id, Post post, User user, String content) {
+        this.id = id;
+        this.post = post;
+        this.user = user;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
