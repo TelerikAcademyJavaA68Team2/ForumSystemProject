c70346a5c6a6ca32fe15d6ff0a1662ad8f3409cc
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-24 12:41:59 +0200

    creating UserController

---
+++ src/main/java/com/example/forumproject/controllers/UserController.java
@@ -0,0 +1,55 @@
+package com.example.forumproject.controllers;
+
+import com.example.forumproject.exceptions.EntityNotFoundException;
+import com.example.forumproject.helpers.UserMapper;
+import com.example.forumproject.models.User;
+import com.example.forumproject.models.UserDto;
+import com.example.forumproject.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserController(UserService userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        try {
+            return userService.getById(id);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> createUser(@Valid @RequestBody UserDto userDto) {
+        try {
+            User user = userMapper.registrationFromDto(userDto);
+            userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body("registration successful");
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+}
