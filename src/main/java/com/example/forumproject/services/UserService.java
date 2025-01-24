129be2defca8d13148a1cdee799a6d1fc71ff484
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-24 11:57:28 +0200

    updating UserService

--- src/main/java/com/example/forumproject/services/UserService.java
+++ src/main/java/com/example/forumproject/services/UserService.java
@@ -6,15 +6,24 @@ import com.example.forumproject.models.User;
 import java.util.List;
 
 public interface UserService {
-    List<User> getAll();
 
-    User getUserById(int id);
+    List<User> getAllUsers();
+
+    void createUser(User user);
+
+    User getById(int userId);
 
     User getByEmail(String email);
 
-    void create(User user);
+    User getByUsername(String username);
 
-    void update(User user, int id);
+    void updateUser(User user);
 
-    void delete(int id);
+    void deleteUser(int userId);
+
+    void promoteToAdmin(int userId);
+
+    void blockUser(int userId);
+
+    void unblockUser(int userId);
 }
