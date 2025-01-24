7be0b2e304fe6ecda0cbc8653a4fc6a86c09dccd
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-01-24 17:13:17 +0200

    feat: implemented delete method in service, as well as a checker for permissions

--- src/main/java/com/example/forumproject/services/PostServiceImpl.java
+++ src/main/java/com/example/forumproject/services/PostServiceImpl.java
@@ -1,7 +1,5 @@
 package com.example.forumproject.services;
 
-import com.example.forumproject.exceptions.DuplicateEntityException;
-import com.example.forumproject.exceptions.EntityNotFoundException;
 import com.example.forumproject.exceptions.UnauthorizedAccessException;
 import com.example.forumproject.models.Post;
 import com.example.forumproject.models.User;
@@ -10,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostServiceImpl implements PostService{
+public class PostServiceImpl implements PostService {
 
     private static final String BLOCKED_CREATION = "Blocked users can't create posts!";
+    public static final String MODIFY_POSTS = "Only admins or the post's creator can modify posts!";
 
     private final PostRepository postRepository;
 
@@ -29,7 +28,7 @@ public class PostServiceImpl implements PostService{
     @Override
     public void create(Post post, User user) {
 
-        if (user.isBlocked()){
+        if (user.isBlocked()) {
             throw new UnauthorizedAccessException(BLOCKED_CREATION);
         }
 
@@ -39,11 +38,21 @@ public class PostServiceImpl implements PostService{
 
     @Override
     public void update(Post post, User user) {
+        checkModifyPermissions(post.getId(), user);
+
 
     }
 
     @Override
     public void delete(int id, User user) {
+        checkModifyPermissions(id, user);
+        postRepository.delete(id);
+    }
 
+    public void checkModifyPermissions(int postId, User user) {
+        Post post = postRepository.getById(postId);
+        if (!(user.isAdmin() || post.getAuthor().equals(user))) {
+            throw new UnauthorizedAccessException(MODIFY_POSTS);
+        }
     }
 }
