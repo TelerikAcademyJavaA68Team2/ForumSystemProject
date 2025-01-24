ebbb4563d643750011e49c847d12d7ec8b6193a6
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-01-24 18:15:23 +0200

    update: implemented updatepostfromdto method in PostMapper for updating a post

--- src/main/java/com/example/forumproject/mappers/PostMapper.java
+++ src/main/java/com/example/forumproject/mappers/PostMapper.java
@@ -3,6 +3,7 @@ package com.example.forumproject.mappers;
 import com.example.forumproject.models.Post;
 import com.example.forumproject.models.User;
 import com.example.forumproject.models.dtos.CreatePostDto;
+import com.example.forumproject.models.dtos.UpdatePostDto;
 import com.example.forumproject.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,14 +20,18 @@ public class PostMapper {
 
     public Post createPostFromDto(CreatePostDto createPostDTO, User author){
         Post post = new Post();
-
         post.setAuthor(author);
-
         post.setTitle(createPostDTO.getTitle());
-
         post.setContent(createPostDTO.getContent());
-
         return post;
     }
+
+    public Post UpdatePostFromDto(UpdatePostDto updatePostDTO, int id){
+        Post postToBeUpdate = postRepository.getById(id);
+        postToBeUpdate.setTitle(updatePostDTO.getTitle());
+        postToBeUpdate.setContent(updatePostDTO.getContent());
+        return postToBeUpdate;
+    }
+
 
 }
