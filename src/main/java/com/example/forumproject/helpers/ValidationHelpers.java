8139da6a64080754d17ce53b075a9d3954576285
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-01-25 17:05:48 +0200

    feat: added permission validators in ValidationHelpers

--- src/main/java/com/example/forumproject/helpers/ValidationHelpers.java
+++ src/main/java/com/example/forumproject/helpers/ValidationHelpers.java
@@ -3,6 +3,9 @@ package com.example.forumproject.helpers;
 import com.example.forumproject.exceptions.DuplicateEntityException;
 import com.example.forumproject.exceptions.EntityNotFoundException;
 import com.example.forumproject.exceptions.InvalidEmailFormatException;
+import com.example.forumproject.exceptions.UnauthorizedAccessException;
+import com.example.forumproject.models.Comment;
+import com.example.forumproject.models.Post;
 import com.example.forumproject.models.User;
 import com.example.forumproject.repositories.UserRepository;
 
@@ -11,6 +14,11 @@ import java.util.regex.Pattern;
 
 
 public class ValidationHelpers {
+
+    private static final String MODIFY_POSTS = "Only admins or the post's creator can modify posts!";
+    private static final String MODIFY_COMMENTS = "Only admins or the comment's creator can modify comments!";
+    private static final String BLOCKED_USER_ERROR_MESSAGE =
+            "Your account is currently blocked! Your permissions to create or edit posts are restricted!";
 
     public static void validateEmailAndUsername(User user, UserRepository userRepository) {
         boolean usernameIsNotValid = true;
@@ -35,13 +43,31 @@ public class ValidationHelpers {
         checkEmail(user.getEmail());
     }
 
-    private static void checkEmail(String email) {
+    public static void checkEmail(String email) {
         String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         Pattern pattern = Pattern.compile(emailRegex);
         Matcher matcher = pattern.matcher(email);
 
         if (!matcher.matches()) {
             throw new InvalidEmailFormatException(email);
+        }
+    }
+
+    public static void validateUserIsAdminOrPostAuthor(Post post, User user) {
+        if (!(user.isAdmin() || post.getAuthor().equals(user))) {
+            throw new UnauthorizedAccessException(MODIFY_POSTS);
+        }
+    }
+
+    public static void validateUserIsAdminOrCommentAuthor(Comment comment, User user) {
+        if (!(user.isAdmin() || comment.getAuthor().equals(user))) {
+            throw new UnauthorizedAccessException(MODIFY_COMMENTS);
+        }
+    }
+
+    public static void validateUserIsNotBlocked(User user) {
+        if (user.isBlocked()) {
+            throw new UnauthorizedAccessException(BLOCKED_USER_ERROR_MESSAGE);
         }
     }
 }
