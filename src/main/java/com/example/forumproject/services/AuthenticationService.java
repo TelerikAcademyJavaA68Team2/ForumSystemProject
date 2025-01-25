6c4415bcd3f5702b5adad2a64ac9d675dcfa77a5
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-25 15:14:01 +0200

    adding AuthenticationService

---
+++ src/main/java/com/example/forumproject/services/AuthenticationService.java
@@ -0,0 +1,60 @@
+package com.example.forumproject.services;
+
+import com.example.forumproject.models.AuthenticationResponse;
+import com.example.forumproject.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthenticationService(UserService userService,
+                                 PasswordEncoder passwordEncoder,
+                                 JwtService jwtService,
+                                 AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    public AuthenticationResponse register(User request) {
+
+        User user = createUserFromRequest(request);
+        userService.save(user);
+
+        String token = jwtService.generateToken(user);
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(User request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(), request.getPassword()));
+
+        User user = userService.loadUserByUsername(request.getUsername());
+        String token = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(token);
+    }
+
+    private User createUserFromRequest(User request) {
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        return user;
+    }
+}
