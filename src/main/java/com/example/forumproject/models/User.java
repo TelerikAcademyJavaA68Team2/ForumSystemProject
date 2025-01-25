9fac472945dfb2ec31af38defd642e8d16b1b147
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-25 15:15:44 +0200

    updating User

--- src/main/java/com/example/forumproject/models/User.java
+++ src/main/java/com/example/forumproject/models/User.java
@@ -1,13 +1,17 @@
 package com.example.forumproject.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,7 +30,6 @@ public class User {
     @Column(name = "username")
     private String username;
 
-    @JsonIgnore
     @Column(name = "password")
     private String password;
 
@@ -45,6 +48,11 @@ public class User {
     public User() {
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public User(
             String firstName,
             String lastName,
@@ -60,21 +68,6 @@ public class User {
         this.isBlocked = false;
     }
 
-    public User(int id,
-                String firstName,
-                String lastName,
-                String email,
-                String username,
-                String password) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.username = username;
-        this.password = password;
-        this.isAdmin = false;
-        this.isBlocked = false;
-    }
 
     public int getId() {
         return id;
@@ -108,6 +101,7 @@ public class User {
         this.email = email;
     }
 
+    @Override
     public String getUsername() {
         return username;
     }
@@ -116,6 +110,7 @@ public class User {
         this.username = username;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
@@ -166,5 +161,30 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id, email, username);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(isAdmin() ? "ROLE_ADMIN" : "ROLE_USER"));
     }
 }
