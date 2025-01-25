bb0e89918b3acc3a2088c31f0a29eaa64f34b08a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-25 15:09:55 +0200

    adding AuthenticationController

---
+++ src/main/java/com/example/forumproject/controllers/AuthenticationController.java
@@ -0,0 +1,41 @@
+package com.example.forumproject.controllers;
+
+import com.example.forumproject.models.AuthenticationResponse;
+import com.example.forumproject.models.User;
+import com.example.forumproject.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+
+@RestController
+@RequestMapping("/api")
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User request) {
+        try {
+            return ResponseEntity.ok(authService.register(request));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody User request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
