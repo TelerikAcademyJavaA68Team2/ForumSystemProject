ebcdb2326e6839661ec8572594b1331bffa0d8e0
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-25 15:13:27 +0200

    updating UserController

--- src/main/java/com/example/forumproject/controllers/UserController.java
+++ src/main/java/com/example/forumproject/controllers/UserController.java
@@ -1,19 +1,15 @@
 package com.example.forumproject.controllers;
 
-import com.example.forumproject.exceptions.DuplicateEntityException;
 import com.example.forumproject.exceptions.EntityNotFoundException;
-import com.example.forumproject.exceptions.InvalidEmailFormatException;
-
 import com.example.forumproject.mappers.UserMapper;
 import com.example.forumproject.models.User;
-
-import com.example.forumproject.models.dtos.UserDto;
 import com.example.forumproject.services.UserService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -23,12 +19,10 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-    private final UserMapper userMapper;
 
     @Autowired
     public UserController(UserService userService, UserMapper userMapper) {
         this.userService = userService;
-        this.userMapper = userMapper;
     }
 
 
@@ -43,19 +37,6 @@ public class UserController {
             return userService.getById(id);
         } catch (EntityNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-        }
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<String> createUser(@Valid @RequestBody UserDto userDto) {
-        try {
-            User user = userMapper.registrationFromDto(userDto);
-            userService.createUser(user);
-            return ResponseEntity.status(HttpStatus.CREATED).body("registration successful");
-        } catch (DuplicateEntityException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
-        } catch (InvalidEmailFormatException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
     }
 }
