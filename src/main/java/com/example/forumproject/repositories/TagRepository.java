c5f9de9d6efaa72122d17dd9ca8e00730cd9eadf
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-26 14:11:52 +0200

    creating TagRepository

---
+++ src/main/java/com/example/forumproject/repositories/TagRepository.java
@@ -0,0 +1,23 @@
+package com.example.forumproject.repositories;
+
+import com.example.forumproject.models.Post;
+import com.example.forumproject.models.Tag;
+
+import java.util.List;
+
+public interface TagRepository {
+
+    boolean checkIfPostIsTagged(int post_id, int tag_id);
+
+    Tag getTag(int post_id, int tag_id);
+
+    List<Tag> getTagsByPostId(int post_id);
+
+    List<Post> getAllPostsByTag(int tag_id);
+
+    void create(Tag tag);
+
+    void update(Tag tag);
+
+    void delete(Tag tag);
+}
