e1064b27969d1693296152e10a4b3efdc545c1eb
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-01-27 00:30:01 +0200

    rename: UserInDto

--- src/main/java/com/example/forumproject/mappers/UserMapper.java
+++ src/main/java/com/example/forumproject/mappers/UserMapper.java
@@ -1,16 +1,16 @@
 package com.example.forumproject.mappers;
 
 import com.example.forumproject.models.User;
-import com.example.forumproject.models.dtos.UserDto;
+import com.example.forumproject.models.dtos.UserInDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
 
-    public User registrationFromDto(UserDto userDto) {
+    public User registrationFromDto(UserInDto userInDto) {
 
-        User user = new User(userDto.getFirstName(), userDto.getLastName(), userDto.getEmail(), userDto.getUsername(), userDto.getPassword());
+        User user = new User(userInDto.getFirstName(), userInDto.getLastName(), userInDto.getEmail(), userInDto.getUsername(), userInDto.getPassword());
         user.setBlocked(false);
         user.setAdmin(false);
         return user;
