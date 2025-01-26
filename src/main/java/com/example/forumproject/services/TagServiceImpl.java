0fdb98163687ae98212e0689245fe2c12d36634e
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-26 14:12:45 +0200

    implementing TagService

---
+++ src/main/java/com/example/forumproject/services/TagServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.forumproject.services;
+
+import com.example.forumproject.models.Post;
+import com.example.forumproject.models.Tag;
+import com.example.forumproject.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagServiceImpl implements TagService {
+
+
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagServiceImpl(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    @Override
+    public List<Tag> getTagsByPostId(int post_id) {
+        return tagRepository.getTagsByPostId(post_id);
+    }
+
+    @Override
+    public List<Post> getAllPostsByTag(int tag_id) {
+        return tagRepository.getAllPostsByTag(tag_id);
+    }
+
+    @Override
+    public void create(Tag tag) {
+        tagRepository.create(tag);
+    }
+
+    @Override
+    public void update(Tag tag) {
+        tagRepository.update(tag);
+    }
+
+    @Override
+    public void delete(Tag tag) {
+        tagRepository.delete(tag);
+    }
+}
