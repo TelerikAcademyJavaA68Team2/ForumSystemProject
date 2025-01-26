4f943fbdceb62aa02a9d8c121fddebfc18e9eff7
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-26 03:06:57 +0200

    adding LikeService

---
+++ src/main/java/com/example/forumproject/services/LikeService.java
@@ -0,0 +1,18 @@
+package com.example.forumproject.services;
+
+import com.example.forumproject.models.Post;
+import com.example.forumproject.models.User;
+
+import java.util.List;
+
+public interface LikeService {
+    int getLikesByPostId(int post_id);
+
+    int getDislikesByPostId(int post_id);
+
+    List<Post> getAllLikedPosts(int user_id);
+
+    List<Post> getAllDislikedPosts(int user_id);
+
+    boolean save(Post post, User user, boolean like);
+}
