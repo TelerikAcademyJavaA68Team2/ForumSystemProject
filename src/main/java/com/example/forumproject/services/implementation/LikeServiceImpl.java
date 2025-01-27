a2def860b1117c254d520831b764f450c98475f5
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-28 01:47:50 +0200

    refactor

--- src/main/java/com/example/forumproject/services/implementation/LikeServiceImpl.java
+++ src/main/java/com/example/forumproject/services/implementation/LikeServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.forumproject.services.implementation;
 
-import com.example.forumproject.models.Like;
+import com.example.forumproject.models.PostLikesDislikes;
 import com.example.forumproject.models.Post;
 import com.example.forumproject.models.User;
 import com.example.forumproject.repositories.contracts.LikesRepository;
@@ -45,27 +45,27 @@ public class LikeServiceImpl implements LikeService {
 
         if (likesRepository.checkIfLikeExists(post.getId(), user.getId())) {
             if (like) {
-                likesRepository.delete(new Like(post, user, true));
+                likesRepository.delete(new PostLikesDislikes(post, user, true));
                 return false; // return false to print correct msg in controller
                 // option A if you like post 2 times you remove the like
                 // option B if you like smth 2 times you get error
                 /*throw new DuplicateEntityException("You have already liked this post!");*/
             }
 
-            likesRepository.update(new Like(post, user, false));
+            likesRepository.update(new PostLikesDislikes(post, user, false));
             return true;
         }
         if (likesRepository.checkIfDislikeExists(post.getId(), user.getId())) {
             if (!like) {
-                likesRepository.delete(new Like(post, user, false));
+                likesRepository.delete(new PostLikesDislikes(post, user, false));
                 return false;
 //                throw new DuplicateEntityException("Unfortunately you can dislike a post only once!");
             }
-            likesRepository.update(new Like(post, user, true));
+            likesRepository.update(new PostLikesDislikes(post, user, true));
             return true;
         }
 
-        likesRepository.create(new Like(post, user, like));
+        likesRepository.create(new PostLikesDislikes(post, user, like));
         return true;
     }
 }
