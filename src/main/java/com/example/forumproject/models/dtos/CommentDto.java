045361a2767930d6354efdc96972e4e2fe39c7f1
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-01-27 16:29:26 +0200

    update: added author to commentdto

--- src/main/java/com/example/forumproject/models/dtos/CommentDto.java
+++ src/main/java/com/example/forumproject/models/dtos/CommentDto.java
@@ -1,9 +1,15 @@
 package com.example.forumproject.models.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class CommentDto {
+
+    @NotBlank(message = "Username can't be empty")
+    @NotNull(message = "Username can't be empty")
+    @Size(min = 2, max = 20, message = "username should be between 2 and 20 symbols!")
+    private String author;
 
     @NotNull(message = "Content can't be empty")
     @Size(min = 1, max = 2048, message = "Content should be between 1 and 2048 symbols")
@@ -14,6 +20,14 @@ public class CommentDto {
 
     public CommentDto(String content) {
         this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public String getContent() {
