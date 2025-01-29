d3d6a6e3a15430a31728f514fd57a1e26e96e164
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-01-29 14:50:56 +0200

    update: update SwaggerConfig file to display all features available on the project as description

--- src/main/java/com/example/forumproject/config/SwaggerConfig.java
+++ src/main/java/com/example/forumproject/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.example.forumproject.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -9,8 +13,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info (
+                contact = @Contact(
+                        name = "Georgi Benchev and Ivan Ivanov",
+                        email = "gega4321@gmail.com ; ivanovivanbusiness@gmail.com"
+                ),
+                description = "This is an **Automobile Forum Management System** developed using the **Spring Boot** framework. The forum allows users to:\n" +
+                        "- **Create posts** related to automobiles.\n" +
+                        "- **Add comments** on posts.\n" +
+                        "- **Add tags** on posts for better filtering.\n" +
+                        "- **Search filtering** on posts based on different attributes.\n" +
+                        "- **React** to posts with likes and dislikes.\n" +
+                        "- **Register and authenticate** using JWT authentication.\n" +
+                        "- **Admins** moderate content to maintain quality.",
+                title = "Automobile Forum Management System API",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        }
+)
 @SecurityScheme(
-        name = "bearerAuth",
+        name = "Bearer Authentication",
+        description = "Login to get your JWT Token",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
@@ -20,10 +49,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components()
-                        .addSecuritySchemes("bearerAuth", new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .addSecuritySchemes("Bearer Authentication", new io.swagger.v3.oas.models.security.SecurityScheme()
                                 .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .description("Login to get your JWT Token")
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
     }
