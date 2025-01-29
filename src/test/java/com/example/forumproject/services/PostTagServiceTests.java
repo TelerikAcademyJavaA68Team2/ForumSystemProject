63ae1a937b94ca2254533fadb8353d56b85b9cfd
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-01-29 21:40:27 +0200

    implementing tests for PostTagService

--- src/test/java/com/example/forumproject/services/PostTagServiceTests.java
+++ src/test/java/com/example/forumproject/services/PostTagServiceTests.java
@@ -1,8 +1,13 @@
 package com.example.forumproject.services;
 
 import com.example.forumproject.Helpers;
+import com.example.forumproject.exceptions.DuplicateEntityException;
 import com.example.forumproject.exceptions.EntityNotFoundException;
+import com.example.forumproject.exceptions.UnauthorizedAccessException;
+import com.example.forumproject.models.Post;
+import com.example.forumproject.models.PostTag;
 import com.example.forumproject.models.Tag;
+import com.example.forumproject.models.User;
 import com.example.forumproject.repositories.PostRepository;
 import com.example.forumproject.repositories.postTagRepository.PostTagRepository;
 import com.example.forumproject.repositories.tagsRepository.TagRepository;
@@ -38,11 +43,6 @@ public class PostTagServiceTests {
     @Test
     public void getTagsByPostId_ShouldReturnTags_When_ValidArgs() {
         // Arrange
-        /*Post mockPost = Helpers.createMockPost();*/
-/*
-        Mockito.when(postRepository.getById(Mockito.any()))
-                .thenReturn(mockPost);
-*/
         Mockito.when(postTagRepository.getTagsByPostId(Mockito.any()))
                 .thenReturn(List.of(Helpers.createMockTag()));
 
@@ -63,18 +63,38 @@ public class PostTagServiceTests {
         Assertions.assertThrows(EntityNotFoundException.class, () -> service.getTagsByPostId(1L));
     }
 
-
     @Test
-    public void getAllPostsByTagId_ShouldReturnPosts_When_ValidArgs() {
+    public void getAllTagsByPostId_ShouldReturnPosts_When_ValidArgs() {
         // Arrange
-        var mockTag = Helpers.createMockTag();
-        Mockito.when(tagRepository.getTagById(Mockito.any()))
-                .thenReturn(mockTag);
         Mockito.when(postTagRepository.getTagsByPostId(Mockito.any()))
                 .thenReturn(List.of(Helpers.createMockTag()));
 
         // Act
         List<Tag> result = service.getTagsByPostId(1L);
+
+        // Assert
+        Assertions.assertEquals(1L, result.size());
+    }
+
+    @Test
+    public void getAllTagsByPostId_ShouldThrowExc_When_PostIdNotValid() {
+        // Arrange
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenThrow(EntityNotFoundException.class);
+
+        // Act, Assert
+        Assertions.assertThrows(EntityNotFoundException.class, () -> service.getTagsByPostId(1L));
+    }
+
+    @Test
+    public void getAllPostsByTagId_ShouldReturnPosts_When_ValidArgs() {
+        // Arrange
+        Post post = Helpers.createMockPost();
+        Mockito.when(postTagRepository.getAllPostsByTagId(Mockito.any()))
+                .thenReturn(List.of(post));
+
+        // Act
+        List<Post> result = service.getAllPostsByTagId(1L);
 
         // Assert
         Assertions.assertEquals(1L, result.size());
@@ -87,8 +107,168 @@ public class PostTagServiceTests {
                 .thenThrow(EntityNotFoundException.class);
 
         // Act, Assert
-        Assertions.assertThrows(EntityNotFoundException.class, () -> service.getTagsByPostId(1L));
+        Assertions.assertThrows(EntityNotFoundException.class, () -> service.getAllPostsByTagId(1L));
     }
 
+    @Test
+    public void checkIfPostIsTagged_ShouldReturnTrue_When_Valid() {
+        // Arrange
+        Post post = Helpers.createMockPost();
+        Mockito.when(postTagRepository.checkIfPostIsTagged(Mockito.any(), Mockito.any()))
+                .thenReturn(true);
 
+        // Act, Assert
+        Assertions.assertTrue(service.checkIfPostIsTagged(1L, 1L));
+    }
+
+    @Test
+    public void checkIfPostIsTagged_ShouldReturnFalse_When_Valid() {
+        // Arrange
+        Post post = Helpers.createMockPost();
+        Mockito.when(postTagRepository.checkIfPostIsTagged(Mockito.any(), Mockito.any()))
+                .thenReturn(false);
+
+        // Act, Assert
+        Assertions.assertFalse(service.checkIfPostIsTagged(1L, 1L));
+    }
+
+    @Test
+    public void createTagOnPost_ShouldThrowExc_When_PostId_Invalid() {
+        // Arrange
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenThrow(EntityNotFoundException.class);
+
+        // Act, Assert
+        Assertions.assertThrows(EntityNotFoundException.class, () -> service.createTagOnPost(1L, "Test"));
+    }
+
+    @Test
+    public void createTagOnPost_ShouldThrowExc_When_UserNotAdminOrPostOwner() {
+        // Arrange
+        User mockUser = Helpers.createMockUser();
+        Post mockPost = Helpers.createMockPost();
+        mockUser.setId(100L);
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenReturn(mockPost);
+        Mockito.when(userService.getAuthenticatedUser()).thenReturn(mockUser);
+
+        // Act, Assert
+        Assertions.assertThrows(UnauthorizedAccessException.class, () -> service.createTagOnPost(Mockito.any(), "Test"));
+    }
+
+    @Test
+    public void createTagOnPost_ShouldThrowExc_When_PostAlreadyTagged() {
+        // Arrange
+        User mockUser = Helpers.createMockAdmin();
+        Post mockPost = Helpers.createMockPost();
+        Tag mockTag = Helpers.createMockTag();
+
+        mockPost.setAuthor(mockUser);
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenReturn(mockPost);
+        Mockito.when(userService.getAuthenticatedUser()).thenReturn(mockUser);
+        Mockito.when(tagRepository.getTagByName(Mockito.any())).thenReturn(mockTag);
+        Mockito.when(postTagRepository.checkIfPostIsTagged(Mockito.any(), Mockito.any()))
+                .thenReturn(true);
+
+        // Act, Assert
+        Assertions.assertThrows(DuplicateEntityException.class, () -> service.createTagOnPost(Mockito.any(), "Test"));
+    }
+
+    @Test
+    public void createTagOnPost_ShouldNotThrowExc_When_ValidArgs() {
+        // Arrange
+        User mockUser = Helpers.createMockAdmin();
+        Post mockPost = Helpers.createMockPost();
+        Tag mockTag = Helpers.createMockTag();
+        mockPost.setAuthor(mockUser);
+        mockUser.setAdmin(true);
+
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenReturn(mockPost);
+        Mockito.when(userService.getAuthenticatedUser())
+                .thenReturn(mockUser);
+        Mockito.when(tagRepository.getTagByName(mockTag.getTagName()))
+                .thenThrow(EntityNotFoundException.class);
+        Mockito.when(postTagRepository.checkIfPostIsTagged(mockPost.getId(), null))
+                .thenReturn(false);
+
+        // Act, Assert
+        Assertions.assertDoesNotThrow(() -> service.createTagOnPost(mockPost.getId(), mockTag.getTagName()));
+    }
+
+    @Test
+    public void updateTagOnPost_ShouldNotThrowExc_When_ValidArgs() {
+        // Arrange
+        User mockUser = Helpers.createMockAdmin();
+        Post mockPost = Helpers.createMockPost();
+        Tag mockTag = Helpers.createMockTag();
+        PostTag mockPostTag = Helpers.createMockPostTag();
+        mockPost.setAuthor(mockUser);
+        mockUser.setAdmin(true);
+
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenReturn(mockPost);
+        Mockito.when(userService.getAuthenticatedUser())
+                .thenReturn(mockUser);
+        Mockito.when(tagRepository.getTagById(1L))
+                .thenReturn(mockTag);
+        Mockito.when(postTagRepository.getPostTag(mockPost.getId(), mockTag.getId()))
+                .thenReturn(mockPostTag);
+        Mockito.when(tagRepository.getTagByName("New Tag"))
+                .thenThrow(EntityNotFoundException.class);
+        Mockito.when(postTagRepository.checkIfPostIsTagged(mockPost.getId(), null))
+                .thenReturn(false);
+
+        // Act, Assert
+        Assertions.assertDoesNotThrow(() -> service.updateTagOnPost(1L, 1L, "New Tag"));
+    }
+
+    @Test
+    public void updateTagOnPost_ShouldThrowExc_When_PostAlreadyTagged() {
+        // Arrange
+        User mockUser = Helpers.createMockAdmin();
+        Post mockPost = Helpers.createMockPost();
+        Tag mockTag = Helpers.createMockTag();
+        Tag mockNewTag = Helpers.createMockTag();
+        mockNewTag.setTagName("New Tag");
+        mockNewTag.setId(2L);
+        PostTag mockPostTag = Helpers.createMockPostTag();
+        mockPost.setAuthor(mockUser);
+        mockUser.setAdmin(true);
+
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenReturn(mockPost);
+        Mockito.when(userService.getAuthenticatedUser())
+                .thenReturn(mockUser);
+        Mockito.when(tagRepository.getTagById(1L))
+                .thenReturn(mockTag);
+        Mockito.when(postTagRepository.getPostTag(mockPost.getId(), mockTag.getId())).thenReturn(mockPostTag);
+        Mockito.when(tagRepository.getTagByName("New Tag")).thenReturn(mockNewTag);
+        Mockito.when(postTagRepository.checkIfPostIsTagged(mockPost.getId(), 2L))
+                .thenReturn(true);
+
+        // Act, Assert
+        Assertions.assertThrows(DuplicateEntityException.class, () -> service.updateTagOnPost(1L, 1L, "New Tag"));
+    }
+
+    @Test
+    public void deleteTagFromPost_ShouldNotThrowExc_When_ValidArgs() {
+        // Arrange
+        User mockUser = Helpers.createMockAdmin();
+        Post mockPost = Helpers.createMockPost();
+        Tag mockTag = Helpers.createMockTag();
+
+        Mockito.when(postRepository.getById(Mockito.any()))
+                .thenReturn(mockPost);
+        Mockito.when(userService.getAuthenticatedUser())
+                .thenReturn(mockUser);
+        Mockito.when(tagRepository.getTagById(1L))
+                .thenReturn(mockTag);
+        Mockito.when(postTagRepository.checkIfPostIsTagged(1L, 1L))
+                .thenReturn(true);
+
+        // Act, Assert
+        Assertions.assertDoesNotThrow(() -> service.deleteTagFromPost(1L, 1L));
+    }
 }
