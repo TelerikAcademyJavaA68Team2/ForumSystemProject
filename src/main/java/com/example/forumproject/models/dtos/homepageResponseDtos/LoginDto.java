739f3e8e9c7f60379c535c86f24da7384c88cf77
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-13 19:49:24 +0200

    adding no args constr to LoginDto

--- src/main/java/com/example/forumproject/models/dtos/homepageResponseDtos/LoginDto.java
+++ src/main/java/com/example/forumproject/models/dtos/homepageResponseDtos/LoginDto.java
@@ -1,16 +1,21 @@
 package com.example.forumproject.models.dtos.homepageResponseDtos;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class LoginDto {
 
-    @NotNull(message = "Username can't be empty")
+    @NotEmpty(message = "Username can't be empty")
     private String username;
 
-    @NotNull(message = "Password can't be empty")
+    @NotEmpty(message = "Password can't be empty")
     @Size(min = 2, max = 20, message = "Password should be between 2 and 20 symbols!")
     private String password;
+
+    public LoginDto() {
+
+    }
 
     public LoginDto(String username, String password) {
         this.username = username;
