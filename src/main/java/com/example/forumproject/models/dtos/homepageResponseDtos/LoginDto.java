7825dd19d0e96539d4e79f1ec02fdbeb56bb7465
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-16 22:37:15 +0200

    setting limit to username

--- src/main/java/com/example/forumproject/models/dtos/homepageResponseDtos/LoginDto.java
+++ src/main/java/com/example/forumproject/models/dtos/homepageResponseDtos/LoginDto.java
@@ -1,12 +1,12 @@
 package com.example.forumproject.models.dtos.homepageResponseDtos;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class LoginDto {
 
     @NotEmpty(message = "Username can't be empty")
+    @Size(min = 2, max = 20, message = "Username should be between 2 and 20 symbols!")
     private String username;
 
     @NotEmpty(message = "Password can't be empty")
@@ -14,7 +14,6 @@ public class LoginDto {
     private String password;
 
     public LoginDto() {
-
     }
 
     public LoginDto(String username, String password) {
