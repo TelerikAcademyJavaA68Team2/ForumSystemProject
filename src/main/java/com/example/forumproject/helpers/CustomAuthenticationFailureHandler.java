4d6069a61af88ff13921f9166b607c6dac9543a0
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-16 22:49:46 +0200

    implementing CustomAuthenticationFailureHandler

--- src/main/java/com/example/forumproject/helpers/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/example/forumproject/helpers/CustomAuthenticationFailureHandler.java
@@ -1,21 +1,25 @@
 package com.example.forumproject.helpers;
 
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        if (exception.getMessage().contains("User is disabled")) {
-            response.sendRedirect("/mvc/blocked");
+
+        if (exception instanceof DisabledException) {
+            response.sendRedirect("/mvc/auth/login?blocked=true");
+        } else {
+            response.sendRedirect("/mvc/auth/login?error=true");
         }
     }
 }
