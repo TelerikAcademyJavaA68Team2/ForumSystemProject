ec5a177e6ac5e0ad99da4b0131d8d35a69fc708e
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-17 15:21:32 +0200

    refactor

--- src/main/java/com/example/forumproject/mappers/PostMapper.java
+++ src/main/java/com/example/forumproject/mappers/PostMapper.java
@@ -51,11 +51,7 @@ public class PostMapper {
         postOutDto.setPost_id(post.getId());
         postOutDto.setAuthor(post.getAuthor().getUsername());
         postOutDto.setAuthorId(post.getAuthor().getId());
-
-        postOutDto.setProfilePicture(
-                post.getAuthor().getPhoto() != null ?
-                        post.getAuthor().getPhoto() : "/images/default-profile-pic.png"
-        );
+        postOutDto.setProfilePicture(post.getAuthor().getPhoto());
 
         postOutDto.setTitle(post.getTitle());
         postOutDto.setContent(post.getContent());
@@ -68,7 +64,7 @@ public class PostMapper {
 
         postOutDto.setComments(commentMapper
                 .commentsToCommentDtos(commentService
-                .getAllCommentsByPostId(post.getId())));
+                        .getAllCommentsByPostId(post.getId())));
 
         postOutDto.setTags(postTagService.getTagsByPostId(post.getId())
                 .stream()
