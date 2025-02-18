b865cd1e2780f9cd885df54be60209e45028bbac
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-18 05:00:17 +0200

    refactor

--- src/main/java/com/example/forumproject/models/dtos/userDtos/RequestUserProfileDto.java
+++ src/main/java/com/example/forumproject/models/dtos/userDtos/RequestUserProfileDto.java
@@ -1,21 +1,41 @@
 package com.example.forumproject.models.dtos.userDtos;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
 public class RequestUserProfileDto {
+
+    @Size(min = 4, max = 32, message = "First name should be between 4 and 32 symbols!")
     private String firstName;
+
+    @Size(min = 4, max = 32, message = "Last name should be between 4 and 32 symbols!")
     private String lastName;
-    @Email
+
+    @Email(message = "Email address is invalid!")
     private String email;
+
+    @URL(message = "Please provide a valid Image URL")
     private String profilePhoto;
+
+    @Size(max = 20, message = "Phone number must not exceed 20 characters")
     private String phoneNumber;
+
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -30,7 +50,15 @@ public class RequestUserProfileDto {
         return profilePhoto;
     }
 
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
