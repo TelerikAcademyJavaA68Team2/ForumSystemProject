a31edad94a158b40e79a9d3a5d7f3425072f2378
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-02-19 12:58:44 +0200

    feat: created PostUpdateDto

---
+++ src/main/java/com/example/forumproject/models/dtos/postDtos/PostUpdateDto.java
@@ -0,0 +1,67 @@
+package com.example.forumproject.models.dtos.postDtos;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public class PostUpdateDto {
+
+    @Positive(message = "Id should be positive")
+    private Long id;
+
+    @NotNull(message = "Title can't be empty")
+    @Size(min = 16, max = 64, message = "Title should be between 16 and 64 symbols")
+    private String title;
+
+    @NotNull(message = "Content can't be empty")
+    @Size(min = 32, max = 8192, message = "Content should be between 32 and 8192 symbols")
+    private String content;
+
+    private List<String> tagNames;
+
+    public PostUpdateDto() {
+    }
+
+    public PostUpdateDto(Long id, String title, String content, List<String> tagNames) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.tagNames = tagNames;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<String> getTagNames() {
+        return tagNames;
+    }
+
+    public void setTagNames(List<String> tagNames) {
+        this.tagNames = tagNames;
+    }
+
+
+}
