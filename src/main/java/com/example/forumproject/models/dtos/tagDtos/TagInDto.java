b07ca91f63039c06d2023cfb2e9b4bf1787dcd4a
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-21 07:05:08 +0200

    update for mvc logic

--- src/main/java/com/example/forumproject/models/dtos/tagDtos/TagInDto.java
+++ src/main/java/com/example/forumproject/models/dtos/tagDtos/TagInDto.java
@@ -1,8 +1,9 @@
 package com.example.forumproject.models.dtos.tagDtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.util.List;
 
 public class TagInDto {
 
@@ -10,19 +11,34 @@ public class TagInDto {
     @Size(min = 3, max = 30, message = "Tag should be between 3 and 30 symbols")
     private String tagName;
 
+    private List<String> tagsList;
+
     public TagInDto() {
     }
 
-    public TagInDto(String tagName) {
-        this.tagName = tagName.toLowerCase();
+    public TagInDto(List<String> tagsList) {
+        this.tagsList = tagsList;
+    }
+
+    public TagInDto(List<String> tagsList, String tagName) {
+        this.tagsList = tagsList;
+        this.tagName = tagName;
     }
 
     public String getTagName() {
-        return tagName.toLowerCase();
+        return tagName;
     }
 
     public void setTagName(String tagName) {
         this.tagName = tagName;
+    }
+
+    public List<String> getTagsList() {
+        return tagsList;
+    }
+
+    public void setTagsList(List<String> tagsList) {
+        this.tagsList = tagsList;
     }
 }
 
