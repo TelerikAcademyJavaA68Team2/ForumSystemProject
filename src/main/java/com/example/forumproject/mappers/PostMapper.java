c9db7a9898d86ed776c2a6ba71d3f9a40a399a52
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-21 06:53:31 +0200

    removed from mapper tags

--- src/main/java/com/example/forumproject/mappers/PostMapper.java
+++ src/main/java/com/example/forumproject/mappers/PostMapper.java
@@ -6,15 +6,12 @@ import com.example.forumproject.models.User;
 import com.example.forumproject.models.dtos.postDtos.PostInDto;
 import com.example.forumproject.models.dtos.postDtos.PostOutDto;
 import com.example.forumproject.models.dtos.postDtos.PostUpdateDto;
-import com.example.forumproject.services.commentService.CommentService;
 import com.example.forumproject.services.PostService;
+import com.example.forumproject.services.commentService.CommentService;
 import com.example.forumproject.services.postTagService.PostTagService;
 import com.example.forumproject.services.reactionService.ReactionService;
-import com.example.forumproject.services.tagService.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class PostMapper {
@@ -24,17 +21,15 @@ public class PostMapper {
     private final ReactionService reactionService;
     private final CommentService commentService;
     private final PostTagService postTagService;
-    private final TagService tagService;
 
     @Autowired
     public PostMapper(CommentMapper commentMapper, PostService postService,
-                      ReactionService reactionService, CommentService commentService, PostTagService postTagService, TagService tagService) {
+                      ReactionService reactionService, CommentService commentService, PostTagService postTagService) {
         this.commentMapper = commentMapper;
         this.postService = postService;
         this.reactionService = reactionService;
         this.commentService = commentService;
         this.postTagService = postTagService;
-        this.tagService = tagService;
     }
 
     public Post createPostFromDto(PostInDto postInDTO, User author) {
@@ -52,46 +47,24 @@ public class PostMapper {
         return postToBeUpdate;
     }
 
-    public PostUpdateDto postToPostUpdateDto(Long id){
+    public PostUpdateDto postToPostUpdateDto(Long id) {
         Post post = postService.getById(id);
 
         PostUpdateDto postUpdateDto = new PostUpdateDto();
-
         postUpdateDto.setId(post.getId());
-
         postUpdateDto.setTitle(post.getTitle());
-
         postUpdateDto.setContent(post.getContent());
-
-        postUpdateDto.setTagNames(postTagService.getTagsByPostId(id)
-                .stream()
-                .map(Tag::getTagName)
-                .toList());
 
         return postUpdateDto;
     }
 
-    public Post dtoToObject(PostUpdateDto updatePostDTO){
+    public Post dtoToObject(PostUpdateDto updatePostDTO) {
         Post post = postService.getById(updatePostDTO.getId());
-
         post.setTitle(updatePostDTO.getTitle());
-
         post.setContent(updatePostDTO.getContent());
-
-        if (updatePostDTO.getTagNames()
-                .stream()
-                .anyMatch(tag -> !tag.matches("[a-z0-9]+") || tag.length() > 15 || tag.length() < 2)){
-            throw new IllegalArgumentException("Tags should be between 2 and 15 symbols (numbers or lowercase letters)!");
-        }
-
-        for (int i = 0; i < updatePostDTO.getTagNames().size(); i++) {
-            postTagService.createTagOnPost(post.getId(), updatePostDTO.getTagNames().get(i));
-        }
 
         return post;
     }
-
-
 
     public PostOutDto postToPostOutDto(Post post) {
         PostOutDto postOutDto = new PostOutDto();
