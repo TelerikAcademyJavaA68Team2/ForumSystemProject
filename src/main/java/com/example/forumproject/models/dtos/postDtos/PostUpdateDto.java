c2030bcb1dff90b61aa3dacc9a496d61a242e9ed
Georgi Benchev <184780317+Georgi-Benchev@users.noreply.github.com>
2025-02-21 07:04:48 +0200

    update

--- src/main/java/com/example/forumproject/models/dtos/postDtos/PostUpdateDto.java
+++ src/main/java/com/example/forumproject/models/dtos/postDtos/PostUpdateDto.java
@@ -1,11 +1,8 @@
 package com.example.forumproject.models.dtos.postDtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-
-import java.util.List;
 
 public class PostUpdateDto {
 
@@ -20,16 +17,14 @@ public class PostUpdateDto {
     @Size(min = 32, max = 8192, message = "Content should be between 32 and 8192 symbols")
     private String content;
 
-    private List<String> tagNames;
 
     public PostUpdateDto() {
     }
 
-    public PostUpdateDto(Long id, String title, String content, List<String> tagNames) {
+    public PostUpdateDto(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
-        this.tagNames = tagNames;
     }
 
     public Long getId() {
@@ -55,14 +50,4 @@ public class PostUpdateDto {
     public void setContent(String content) {
         this.content = content;
     }
-
-    public List<String> getTagNames() {
-        return tagNames;
-    }
-
-    public void setTagNames(List<String> tagNames) {
-        this.tagNames = tagNames;
-    }
-
-
 }
