04c0a9672b691bdd5fc1fd3a239e2b4b2c1b3d53
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-19 13:52:55 +0200

    refactor
    (cherry picked from commit bcd630ba9731d7e7c64e990c41a58d55a2f87830)

--- src/main/java/com/example/forumproject/config/SwaggerConfig.java
+++ src/main/java/com/example/forumproject/config/SwaggerConfig.java
@@ -34,6 +34,10 @@ import org.springframework.context.annotation.Configuration;
                 @Server(
                         description = "Local ENV",
                         url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Heroku Deployment",
+                        url = "https://your-heroku-app.herokuapp.com"
                 )
         }
 )
