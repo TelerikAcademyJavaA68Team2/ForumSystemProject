27f51e7e731f94ba482424bc18f8e5c170b21469
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2025-03-22 19:40:15 +0200

    update: added heroku url to swagger documentation

--- src/main/java/com/example/forumproject/config/SwaggerConfig.java
+++ src/main/java/com/example/forumproject/config/SwaggerConfig.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
